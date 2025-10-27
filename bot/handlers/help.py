fbaf5004ea4899b8c6e94a831cd6d532f3e698a1
Aleksandr Arsentev <arsenntv@gmail.com>
2025-10-27 13:35:22 +0800

    Help command handler added

---
+++ bot/handlers/help.py
@@ -0,0 +1,23 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from bot.stats import stats
+
+router = Router()
+
+HELP = (
+    "Справка по командам:\n"
+    "/todo add <текст>\n"
+    "/todo list\n"
+    "/todo done <id>\n"
+    "/weather <город>\n"
+    "/rate <BASE> <SYM1,SYM2,...>\n"
+    "/fileinfo\n"
+    "/stats"
+)
+
+
+@router.message(F.text.startswith("/help"))
+async def help_cmd(message: Message) -> None:
+    stats.seen_user(message.from_user.id)
+    stats.bump("help")
+    await message.answer(HELP)
