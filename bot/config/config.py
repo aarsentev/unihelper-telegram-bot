62e2e014bdd306c44c4fe0d1d174313ef2d66b5b
Aleksandr Arsentev <arsenntv@gmail.com>
2025-10-27 14:09:41 +0800

    Data location mismatch fixed + gitignore updated

--- bot/config/config.py
+++ bot/config/config.py
@@ -1,14 +1,17 @@
 import os
+from pathlib import Path
 from dataclasses import dataclass
 from dotenv import load_dotenv
 
 load_dotenv()
 
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+
 
 @dataclass(frozen=True)
 class Settings:
     bot_token: str = os.getenv("BOT_TOKEN", "")
-    db_path: str = os.getenv("DB_PATH", "todo.sqlite3")
+    db_path: str = os.getenv("DB_PATH", str(PROJECT_ROOT / "data" / "todo.sqlite3"))
 
 
 settings = Settings()
